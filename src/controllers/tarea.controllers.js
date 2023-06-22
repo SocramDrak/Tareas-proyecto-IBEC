25b820570b677e30b0788bb04f04830dd0e84f8e
marcos duran henao <socramdark1@gmail.com>
2023-06-21 22:31:51 -0300

    ya se creo todo lo que se pide para la enrega 	modified:   src/controllers/tarea.controllers.js 	modified:   src/routes/tarea.routes.js

--- src/controllers/tarea.controllers.js
+++ src/controllers/tarea.controllers.js
@@ -1,22 +1,110 @@
+
+const tareaModels = require('../models/tarea.models');
 const Tarea = require('../models/tarea.models');
 
-const crearTarea = async (rec,res)=>{
-   try {
-    const newTarea = await Tarea.create(rec.body);
-    
-    return res.status(200).json({
-        ok:true,
-        msg: 'Tarea creada excitosamente',
-        data: newTarea
-    })
-   } catch (error){
-    console.error(error);   
-    return res.status(500).json({
-        ok:false,
-        msg: 'Error creando producto'            
-    })
-    
-   }
-}
 
-module.exports={crearTarea}
+
+const crearTarea = async (req, res) => {
+    const { titulo, descripcion } = req.body;
+  
+    // Verificar si ya existe una tarea con el mismo título
+    Tarea.findOne({ titulo })
+      .then(tareaExistente => {
+        if (tareaExistente) {
+          return res.status(409).json({ mensaje: 'Ya existe una tarea con el mismo título.' });
+        }
+  
+        // Crear una nueva tarea
+        const nuevaTarea = new Tarea({ titulo, descripcion });
+  
+        // Guardar la tarea en la base de datos
+        nuevaTarea.save()
+          .then(tareaCreada => {
+            res.json(tareaCreada);
+          })
+          .catch(error => {
+            console.error(error);
+            res.status(500).json({ mensaje: 'Ocurrió un error al crear la tarea.' });
+          });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Ocurrió un error al verificar la existencia de la tarea.' });
+      });
+  };
+  
+const verTareas = async (req, res) => {
+    // Buscar todas las tareas en la base de datos
+    Tarea.find({})
+      .then(tareas => {
+        res.status(200).json(tareas);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Ocurrió un error al obtener las tareas.' });
+      });
+  };
+
+const buscarTarea = async (req, res) => {
+    const { titulo } = req.body;
+  
+    // Buscar la tarea por título en la base de datos
+    Tarea.findOne({ titulo })
+      .then(tareaEncontrada => {
+        if (!tareaEncontrada) {
+          return res.status(404).json({ mensaje: 'Tarea no encontrada.' });
+        }
+        
+        // Devolver la tarea encontrada 
+        res.status(200).json(tareaEncontrada);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Ocurrió un error al buscar la tarea.' });
+      });
+  };
+
+  const editarTarea = async (req, res) => {
+    const { titulo, nuevoTitulo, nuevaDescripcion } = req.body;
+  
+    // Buscar y actualizar la tarea por el título en la base de datos
+    Tarea.findOneAndUpdate({ titulo }, { titulo: nuevoTitulo, descripcion: nuevaDescripcion }, { new: true })
+      .then(tareaActualizada => {
+        if (!tareaActualizada) {
+          return res.status(404).json({ mensaje: 'Tarea no encontrada.' });
+        }
+  
+        // Devolver la tarea actualizada 
+        res.status(200).json(tareaActualizada);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Ocurrió un error al editar la tarea.' });
+      });
+  };
+
+  const eliminarTarea = async  (req, res) => {
+    const { titulo } = req.body;
+  
+    // Verificar que se haya proporcionado un título que excista
+    if (!titulo) {
+      return res.status(400).json({ mensaje: 'Se requiere el título de la tarea.' });
+    }
+  
+    // Eliminar la tarea por título en la base de datos
+    Tarea.findOneAndRemove({ titulo })
+      .then(tareaEliminada => {
+        if (!tareaEliminada) {
+          return res.status(404).json({ mensaje: 'Tarea no encontrada.' });
+        }
+        
+        // Devolver la tarea eliminada en formato JSON
+        res.status(200).json(tareaEliminada);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Ocurrió un error al eliminar la tarea.' });
+      });
+  };
+
+module.exports={verTareas,crearTarea,buscarTarea,editarTarea,eliminarTarea}
--- src/routes/tarea.routes.js
+++ src/routes/tarea.routes.js
@@ -1,10 +1,22 @@
 const { Router } = require("express");
-const { crearTarea } = require("../controllers/tarea.controllers");
+const { crearTarea,verTareas,buscarTarea,editarTarea, eliminarTarea } = require("../controllers/tarea.controllers");
 
 const router = Router();
 
 //crear tarea  http://localhost:8080/api/v1/alta
 
-router.post('/alta',crearTarea)
+router.post('/alta',crearTarea);
+
+//ver tarea  http://localhost:8080/api/v1/vertareas
+router.get('/vertareas',verTareas);
+
+//buscar tarea  http://localhost:8080/api/v1/buscar
+router.get('/buscar',buscarTarea);
+
+//editar tarea  http://localhost:8080/api/v1/editar
+router.put('/editar',editarTarea);
+
+//eliminar tarea  http://localhost:8080/api/v1/eliminar
+router.delete('/eliminar',eliminarTarea);
 
 module.exports=router;
